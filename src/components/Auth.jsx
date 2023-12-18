9197b1bef647d490b03a8b13264083d3d18b1bde
AdminForIinRage <bhattaraianjesh123@gmail.com>
2023-12-18 14:17:12 +1030

    good auth

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,50 +1,105 @@
-import { useState } from "react";
-
-import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useState, useEffect } from "react";
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut as firebaseSignOut,
+} from "firebase/auth";
 
 export default function Auth() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // const [error, setError] = useState("");
-  // const [success, setSuccess] = useState("");
-  // const [loading, setLoading] = useState(false);
-  // const [showPassword, setShowPassword] = useState(false);
-  // const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
 
-  console.log(auth?.currentUser?.email);
-
-  const SignIn = async () => {
-
-    try {
-        await createUserWithEmailAndPassword(auth, email, password)
-    } catch (error) {
-        console.log(error);
-        alert(error.message)
-    
-        
+  // Function to fetch user profile information
+  const fetchUserProfile = async (user) => {
+    if (user) {
+      const displayName = user.displayName;
+      setUserName(displayName);
+    } else {
+      setUserName("");
     }
-
-
   };
+
+  // Function to handle Google sign-in
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const user = result.user;
+      setCurrentUser(user);
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
+  // Function to handle sign-out
+  const handleSignOut = async () => {
+    try {
+      await firebaseSignOut(auth);
+      setCurrentUser(null);
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
+  // Function to handle regular sign-in
+  const signIn = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      setCurrentUser(user);
+    } catch (error) {
+      console.log(error);
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      fetchUserProfile(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <div>
-      <input
-      type="email"
-        placeholder="email"
-        onChange={(e) => {
-          setEmail(e.target.value);
-          
-        }}
-      />
-      <input
-       type="password"
-        placeholder="Password"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-      <button onClick={SignIn}>Sign In</button>
+      {currentUser ? (
+        <div>
+          <p>Welcome {email}</p>
+          {userName && <p>Hello, {userName}</p>}
+          <button onClick={handleSignOut}>Sign Out</button>
+        </div>
+      ) : (
+        <div>
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button onClick={signIn}>Sign In</button>
+          <button onClick={signInWithGoogle}>Sign In With Google</button>
+        </div>
+      )}
     </div>
   );
 }
